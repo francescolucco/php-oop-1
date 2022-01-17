020ee1c3e03fc595f7b8b2eeb8dc420f494d65a7
francescolucco <francescolucco@googlemail.com>
2022-01-17 18:55:01 +0100

    box-film

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ metodi d’istanza che abbiamo visto stamattina e create un file `index.php` in 
 
 <?php
 
-require_once __DIR__."/db.php";
-require_once __DIR__."/movie.php";
+require_once __DIR__ . "/db.php";
+require_once __DIR__ . "/movie.php";
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,6 +26,7 @@ require_once __DIR__."/movie.php";
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
    <title>Movies</title>
 </head>
+
 <body>
    <header class="d-flex justify-content-center align-items-center">
       <i class="fa-solid fa-film"></i>
@@ -35,27 +37,37 @@ require_once __DIR__."/movie.php";
       <i class="fa-solid fa-film"></i>
       <i class="fa-solid fa-film"></i>
    </header>
-   
-<i class="fa-solid fa-video"></i>
-   <?php foreach($movies as $movie): 
-      $new_movie = new Movie($movie['titolo'], $movie['genere']);
-      $new_movie->genere = $movie['genere'];
-      $new_movie->durata = $movie['durata'];
-      $new_movie->anno_produzione = $movie['anno_produzione'];
-      $new_movie->voto_MYmovies = $movie['voto_MYmovies'];
-      $new_movie->image = $movie['image'];
-      ?>
-   <div>
-      <h1><?php echo $new_movie->titolo; ?></h1>
-      <img src="<?php echo $new_movie->image; ?>" alt="">
-      <p>Genere: <strong><?php echo $new_movie->genere; ?></strong></p>
-      <p>Durata: <strong><?php echo $new_movie->durata; ?> minuti</strong></p>
-      <p>Anno di produzione: <strong></strong><?php echo $new_movie->anno_produzione; ?></p>
-      <p>Voto su MVBD: <strong><?php echo $new_movie->voto_MYmovies; ?></strong></p>
-      <p><strong><?php echo $new_movie->getGiudizioCritica(); ?></strong></p>
-   </div>
-   <?php endforeach; ?>
+
+   <main>
+      <div class="container">
+         <div class="row">
+            
+            <?php foreach ($movies as $movie) :
+                     $new_movie = new Movie($movie['titolo'], $movie['genere']);
+                     $new_movie->genere = $movie['genere'];
+                     $new_movie->durata = $movie['durata'];
+                     $new_movie->anno_produzione = $movie['anno_produzione'];
+                     $new_movie->voto_MYmovies = $movie['voto_MYmovies'];
+                     $new_movie->image = $movie['image'];
+                     ?>
+                     <div class="col-6">
+                     <div class="box">
+                     <i class="fa-solid fa-video"></i>
+                     <h1><?php echo $new_movie->titolo; ?></h1>
+                     <img src="<?php echo $new_movie->image; ?>" alt="">
+                     <p>Genere: <strong><?php echo $new_movie->genere; ?></strong></p>
+                     <p>Durata: <strong><?php echo $new_movie->durata; ?> minuti</strong></p>
+                     <p>Anno di produzione: <strong></strong><?php echo $new_movie->anno_produzione; ?></p>
+                     <p>Voto su MVBD: <strong><?php echo $new_movie->voto_MYmovies; ?></strong></p>
+                     <p><strong><?php echo $new_movie->getGiudizioCritica(); ?></strong></p>
+                  </div>
+               </div>
+                  <?php endforeach; ?>
+         </div>
+      </div>
+   </main>
 
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
+
 </html>
